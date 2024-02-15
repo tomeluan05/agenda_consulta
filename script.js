3e0cb3b08eeb1dc132ec6fc264ae4675c33aa15c
Tome Luan <emot-naul@hotmail.com>
2024-02-15 09:32:15 -0300

    ajustando a distancia do menu + quando ativo

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const botao_mais = document.querySelector('.botao_mais')
 const container = document.querySelector('.container')
 
 botao_mais.addEventListener('click',()=>{
-    if(container.style.display == 'block'){
+    if(container.style.display == 'flex'){
         container.style.display = 'none';
     }
     else{
-        container.style.display = 'block';
+        container.style.display = 'flex';
     }
 })
